c7b01315853d2341d0038c3c664ed9985812000e
Rajtilak Pandey <rajtilakpandey16@gmail.com>
2024-11-24 15:35:05 +0530

    dashboard done

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,101 +1,101 @@
 import Image from "next/image";
+import Link from "next/link";
+import { Models } from "node-appwrite";
 
-export default function Home() {
+import ActionDropdown from "@/components/ActionDropdown";
+import { Chart } from "@/components/Chart";
+import { FormattedDateTime } from "@/components/FormattedDateTime";
+import { Thumbnail } from "@/components/Thumbnail";
+import { Separator } from "@/components/ui/separator";
+import { getFiles, getTotalSpaceUsed } from "@/lib/actions/file.actions";
+import { convertFileSize, getUsageSummary } from "@/lib/utils";
+
+const Dashboard = async () => {
+  // Parallel requests
+  const [files, totalSpace] = await Promise.all([
+    getFiles({ types: [], limit: 10 }),
+    getTotalSpaceUsed(),
+  ]);
+
+  // Get usage summary
+  const usageSummary = getUsageSummary(totalSpace);
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="dashboard-container">
+      <section>
+        <Chart used={totalSpace.used} />
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        {/* Uploaded file type summaries */}
+        <ul className="dashboard-summary-list">
+          {usageSummary.map((summary) => (
+            <Link
+              href={summary.url}
+              key={summary.title}
+              className="dashboard-summary-card"
+            >
+              <div className="space-y-4">
+                <div className="flex justify-between gap-3">
+                  <Image
+                    src={summary.icon}
+                    width={100}
+                    height={100}
+                    alt="uploaded image"
+                    className="summary-type-icon"
+                  />
+                  <h4 className="summary-type-size">
+                    {convertFileSize(summary.size) || 0}
+                  </h4>
+                </div>
+
+                <h5 className="summary-type-title">{summary.title}</h5>
+                <Separator className="bg-light-400" />
+                <FormattedDateTime
+                  date={summary.latestDate}
+                  className="text-center"
+                />
+              </div>
+            </Link>
+          ))}
+        </ul>
+      </section>
+
+      {/* Recent files uploaded */}
+      <section className="dashboard-recent-files">
+        <h2 className="h3 xl:h2 text-light-100">Recent files uploaded</h2>
+        {files.documents.length > 0 ? (
+          <ul className="mt-5 flex flex-col gap-5">
+            {files.documents.map((file: Models.Document) => (
+              <Link
+                href={file.url}
+                target="_blank"
+                className="flex items-center gap-3"
+                key={file.$id}
+              >
+                <Thumbnail
+                  type={file.type}
+                  extension={file.extension}
+                  url={file.url}
+                />
+
+                <div className="recent-file-details">
+                  <div className="flex flex-col gap-1">
+                    <p className="recent-file-name">{file.name}</p>
+                    <FormattedDateTime
+                      date={file.$createdAt}
+                      className="caption"
+                    />
+                  </div>
+                  <ActionDropdown file={file} />
+                </div>
+              </Link>
+            ))}
+          </ul>
+        ) : (
+          <p className="empty-list">No files uploaded</p>
+        )}
+      </section>
     </div>
   );
-}
+};
+
+export default Dashboard;
